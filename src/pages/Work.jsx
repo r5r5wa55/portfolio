143c2db53289fbbeaf4517f32e1f223a9065b200
werapol <r5r5wa55@hotmail.com>
2024-04-30 14:54:59 +0700

    md:Timeline

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -3,7 +3,57 @@ import React from 'react'
 export default function Work() {
   return (
     <div>
-      Work
+      <div className="bg-wecolor h-60 flex flex-col justify-center items-center text-white  mb-0 mt-16">
+        <h2 className="font-bold text-2xl">HOME.WORK</h2>
+        <h1 className="text-6xl font-extrabold text-work mb-32">Experience</h1>
+      </div>
+
+      <div className="text-white max-w-7xl mx-auto justify-center flex">
+        <div className="max-w-[1200px] grid md:grid-cols-2 ">
+          <div className="">
+            <div className="border-2 border-white p-10 md:mx-10 md:mb-20 border-r relative mb-10 mx-10">
+              <div className="w-5 h-5 bg-white absolute md:-right-16 md:top-[45%] rounded-full bottom-0 "></div>
+              <div className="border-b  w-14 border-white absolute md:top-[50%] md:-right-14" ></div>
+              <div className="h-96 top-0 border-r-2 border-white absolute md:-right-14"></div>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita error nemo porro dolores saepe impedit, eum obcaecati libero voluptates voluptas nihil aut maxime corporis dolorem, eaque delectus culpa enim doloribus.
+            </div>
+          </div>
+          
+          <div className=""></div>
+          <div className=""></div>
+
+          <div className="">
+            <div className="border-2 border-white p-10 md:mx-10 md:ml-[67px] md:mb-20 border-r relative mb-10 mx-10">
+              <div className="w-5 h-5 bg-white absolute md:-left-16 md:top-[45%] rounded-full"></div>
+              <div className="border-b  w-14 border-white absolute md:top-[50%] md:-left-14" ></div>
+              <div className="h-96 top-0 border-r-2 border-white absolute md:-left-14"></div>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita error nemo porro dolores saepe impedit, eum obcaecati libero voluptates voluptas nihil aut maxime corporis dolorem, eaque delectus culpa enim doloribus.
+            </div>
+          </div>
+
+          <div className="">
+            <div className="border-2 border-white p-10 md:mx-10 md:mb-20 border-r relative mb-10 mx-10">
+              <div className="w-5 h-5 bg-white absolute md:-right-16 md:top-[45%] rounded-full"></div>
+              <div className="border-b  w-14 border-white absolute md:top-[50%] md:-right-14" ></div>
+              <div className="h-96 top-0 border-r-2 border-white absolute md:-right-14"></div>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita error nemo porro dolores saepe impedit, eum obcaecati libero voluptates voluptas nihil aut maxime corporis dolorem, eaque delectus culpa enim doloribus.
+            </div>
+          </div>
+
+          <div className=""></div>
+          <div className=""></div>
+          <div className="">
+
+            <div className="border-2 border-white p-10 md:mx-10 md:ml-[67px] md:mb-20 border-r relative mb-10 mx-10">
+              <div className="w-5 h-5 bg-white absolute md:-left-16 md:top-[45%] rounded-full"></div>
+              <div className="border-b  w-14 border-white absolute md:top-[50%] md:-left-14" ></div>
+              <div className="h-64 top-0 border-r-2 border-white absolute md:-left-14"></div>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita error nemo porro dolores saepe impedit, eum obcaecati libero voluptates voluptas nihil aut maxime corporis dolorem, eaque delectus culpa enim doloribus.
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
